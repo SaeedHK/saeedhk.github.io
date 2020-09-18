e5f4a02f26804171669046e4658fca8e3dec41af
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2020-09-18 22:18:37 +0200

    feat: add a header image to blogs

--- src/components/BlogLayout.js
+++ src/components/BlogLayout.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { Header, Container, Divider } from "semantic-ui-react";
+import { Header, Container, Divider, Segment } from "semantic-ui-react";
 import Layout from "./Layout";
+import backgroundSVG from "../images/background.svg";
 
 const BlogLayout = ({ title, author, children }) => (
   <Layout>
+    <Segment
+      style={{
+        backgroundImage: `url(${backgroundSVG})`,
+        backgroundRepeat: "no-repeat",
+        minHeight: 150,
+        padding: "1em 0em",
+        marginBottom: "3em",
+      }}
+      vertical
+    />
     <Container>
       <Header as="h1">
         {title}
