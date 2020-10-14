e7317a1bda0ca97f4f02edd40ef5ca91318d3822
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2020-10-14 11:54:17 +0200

    Feat: add github,twitter, linked links to top menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,11 @@ import { Container, Menu } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const twitterUrl = "https://twitter.com/hadikhanloo";
+  const linkedinUrl = "https://www.linkedin.com/in/saeed-hadikhanloo-24a46052/";
+  const githubUrl = "https://github.com/saeedhk";
   return (
-    <Menu secondary style={{ margin: "0.5em 0em" }}>
+    <Menu secondary style={{ margin: "1.5em 0em" }}>
       <Container>
         <Menu.Item as={NavLink} exact to="/">
           Home
@@ -18,6 +21,12 @@ const Navbar = () => {
         <Menu.Item as={NavLink} exact to="/contact">
           Contact
         </Menu.Item>
+
+        <Menu.Menu position="right">
+          <Menu.Item as="a" href={twitterUrl} icon="twitter" />
+          <Menu.Item as="a" href={githubUrl} icon="github" />
+          <Menu.Item as="a" href={linkedinUrl} icon="linkedin" />
+        </Menu.Menu>
       </Container>
     </Menu>
   );
