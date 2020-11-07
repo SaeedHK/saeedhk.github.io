1e351847671670c7bad7432c9272d95d82c959fc
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2020-11-07 21:41:11 +0100

    Create Blog routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home.js";
-import Blog from "./pages/Blog.js";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import BlogPost from "./components/BlogPost";
 import About from "./pages/About";
-import Error from "./pages/Error.js";
+import Error from "./pages/Error";
 
 export default class App extends Component {
   render() {
@@ -12,7 +13,8 @@ export default class App extends Component {
         <div>
           <Switch>
             <Route path="/" component={Home} exact />
-            <Route path="/blog" component={() => <Blog id="1" />} exact />
+            <Route path="/blog" component={() => <Blog />} exact />
+            <Route path="/blog/:id" component={() => <BlogPost />} exact />
             <Route path="/about" component={About} exact />
             <Route component={Error} />
           </Switch>
--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import Markdown from "markdown-to-jsx";
 import { Header, Container, Divider } from "semantic-ui-react";
 
@@ -21,58 +22,50 @@ const BlogLayout = ({ title, author, children }) => (
   </Layout>
 );
 
-class BlogPost extends Component {
-  constructor(props) {
-    super(props);
-    const blogMeta = blogsMeta.find((blog) => blog.id === props.id);
-    this.fileName = blogMeta ? blogMeta.file : null;
-    this.title = blogMeta ? blogMeta.title : null;
-    this.author = blogMeta ? blogMeta.author : null;
-    this.state = { md: "", err: false };
-  }
+const BlogPost = () => {
+  const { id } = useParams();
+  const blogMeta = blogsMeta.find((blog) => blog.id === id);
+  const fileName = blogMeta ? blogMeta.file : null;
+  const title = blogMeta ? blogMeta.title : null;
+  const author = blogMeta ? blogMeta.author : null;
+  const [md, setMD] = useState("");
 
-  componentWillMount() {
-    if (this.fileName) {
-      const filePath = require("../blogs/" + this.fileName);
+  useEffect(() => {
+    if (fileName) {
+      const filePath = require("../blogs/" + fileName);
       fetch(filePath)
         .then((res) => res.text())
         .then((md) => {
-          this.setState({ md });
+          setMD(md);
         });
     } else {
-      this.setState({
-        err: true,
-        md:
-          "#Error \n The blog is not found. Use the blog links appeared in the blog list.",
-      });
+      setMD(
+        "#Error \n The blog is not found. Use the blog links appeared in the blog list."
+      );
     }
-  }
+  });
 
-  render() {
-    let { md } = this.state;
-
-    return (
-      <BlogLayout title={this.title} author={this.author}>
-        <Markdown
-          options={{
-            overrides: {
-              Code: {
-                component: Code,
-              },
-              Math: {
-                component: MathMD,
-              },
-              p: {
-                component: Paragraph,
-              },
+  return (
+    <BlogLayout title={title} author={author}>
+      <Markdown
+        options={{
+          overrides: {
+            Code: {
+              component: Code,
             },
-          }}
-        >
-          {md}
-        </Markdown>
-      </BlogLayout>
-    );
-  }
-}
+            Math: {
+              component: MathMD,
+            },
+            p: {
+              component: Paragraph,
+            },
+          },
+        }}
+      >
+        {md}
+      </Markdown>
+    </BlogLayout>
+  );
+};
 
 export default BlogPost;
