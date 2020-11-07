0267c816a50da372abbdc2086c19a63584412741
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2020-11-07 21:49:57 +0100

    Fix Blog Card button

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -2,14 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { Segment, Header, Divider, Button } from "semantic-ui-react";
 
-const BlogCard = ({ blog: { author, title, abstract } }) => {
+const BlogCard = ({ blog: { author, title, abstract, id } }) => {
   return (
     <Segment secondary>
       <Header as="h3">{title}</Header>
       <Header as="h4">{author}</Header>
       <Divider />
       <p>{abstract}</p>
-      <Button as={NavLink} exact to="/blog">
+      <Button as={NavLink} exact to={`/blog/${id}`}>
         Read more
       </Button>
     </Segment>
