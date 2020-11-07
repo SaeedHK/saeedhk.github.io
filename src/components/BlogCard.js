13fd394e237b770615415453c96c7c244bd9b16e
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2020-11-07 21:00:06 +0100

    Create BlogCard

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -2,13 +2,13 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { Segment, Header, Divider, Button } from "semantic-ui-react";
 
-const BlogCard = ({ author, title, abstract }) => {
+const BlogCard = ({ blog: { author, title, abstract } }) => {
   return (
     <Segment secondary>
       <Header as="h3">{title}</Header>
       <Header as="h4">{author}</Header>
       <Divider />
-      {abstract}
+      <p>{abstract}</p>
       <Button as={NavLink} exact to="/blog">
         Read more
       </Button>
