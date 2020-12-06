5314cfd182b8c7f102ad8ac41cebbfea2086ef51
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2020-12-06 16:31:50 +0100

    add countries dropdown

--- src/pages/Covid19.js
+++ src/pages/Covid19.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout";
-import { Container, Table, Flag, Header } from "semantic-ui-react";
+import { Container, Dropdown, Table, Flag, Header } from "semantic-ui-react";
 import moment from "moment";
 import { Line } from "react-chartjs-2";
 
@@ -24,7 +24,6 @@ const Covid19 = () => {
       .then((resp) => resp.json())
       .then((d) => {
         setCountryData(d);
-        console.log(d);
       })
       .catch((e) => {
         console.log(e);
@@ -32,9 +31,48 @@ const Covid19 = () => {
       });
   }, [country]);
 
+  const countryOptions = [
+    { key: "af", value: "af", flag: "af", text: "Afghanistan" },
+    { key: "ax", value: "ax", flag: "ax", text: "Aland Islands" },
+    { key: "al", value: "al", flag: "al", text: "Albania" },
+    { key: "dz", value: "dz", flag: "dz", text: "Algeria" },
+    { key: "as", value: "as", flag: "as", text: "American Samoa" },
+    { key: "ad", value: "ad", flag: "ad", text: "Andorra" },
+    { key: "ao", value: "ao", flag: "ao", text: "Angola" },
+    { key: "ai", value: "ai", flag: "ai", text: "Anguilla" },
+    { key: "ag", value: "ag", flag: "ag", text: "Antigua" },
+    { key: "ar", value: "ar", flag: "ar", text: "Argentina" },
+    { key: "am", value: "am", flag: "am", text: "Armenia" },
+    { key: "aw", value: "aw", flag: "aw", text: "Aruba" },
+    { key: "au", value: "au", flag: "au", text: "Australia" },
+    { key: "at", value: "at", flag: "at", text: "Austria" },
+    { key: "az", value: "az", flag: "az", text: "Azerbaijan" },
+    { key: "bs", value: "bs", flag: "bs", text: "Bahamas" },
+    { key: "bh", value: "bh", flag: "bh", text: "Bahrain" },
+    { key: "bd", value: "bd", flag: "bd", text: "Bangladesh" },
+    { key: "bb", value: "bb", flag: "bb", text: "Barbados" },
+    { key: "by", value: "by", flag: "by", text: "Belarus" },
+    { key: "be", value: "be", flag: "be", text: "Belgium" },
+    { key: "bz", value: "bz", flag: "bz", text: "Belize" },
+    { key: "bj", value: "bj", flag: "bj", text: "Benin" },
+  ];
+
+  const CountryDropDown = () => (
+    <Dropdown
+      clearable
+      fluid
+      multiple
+      search
+      selection
+      options={countryOptions}
+      placeholder="Select Country"
+    />
+  );
+
   return (
     <Layout>
       <Container>
+        <CountryDropDown />
         <div id="country-plot">
           <Line
             data={{
